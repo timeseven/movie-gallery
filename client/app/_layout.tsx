5c008d168494f28dc7b0cf9fac4feabffde4f1c3
Even Qian <evenwin88@gmail.com>
2023-11-05 15:58:49 +1100

    fix: change api

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -8,7 +8,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // Initialize Apollo Client
 const client = new ApolloClient({
-  uri: "http://ec2-13-211-240-186.ap-southeast-2.compute.amazonaws.com:5000/graphql",
+  uri: "http://ec2-13-211-240-186.ap-southeast-2.compute.amazonaws.com/graphql",
   cache: new InMemoryCache(),
 });
 
